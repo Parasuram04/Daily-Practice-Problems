5936651cbf1447e2da188a97b62f9cec15234276
Parasuram04 <118524478+Parasuram04@users.noreply.github.com>
2024-09-09 23:08:21 +0530

    Update Solution.cs

--- Day-1/Solution.cs
+++ Day-1/Solution.cs
@@ -1,3 +1,63 @@
+/*
+Problem Description:
+Ashish has a collection of N strings, where some strings may be duplicated. 
+He is tasked with finding the kth unique string from this collection. 
+If the number of unique strings is less than k, return an empty string.
+
+Input Format:
+- The first line contains an integer N, which represents the number of strings.
+- The next N lines each contain a string.
+- The last line contains an integer k, which represents the position of the unique string to be returned.
+
+Output Format:
+- Output the kth unique string. If there are fewer than k unique strings, return an empty string.
+
+Constraints:
+- 1 <= N <= 10^3
+- 1 <= String.length() <= 10^3
+
+Approach:
+1. Read the input strings and maintain their frequency of occurrence using a dictionary or hash map.
+2. Extract the unique strings from the collection by filtering out duplicates.
+3. If the number of unique strings is greater than or equal to k, return the kth unique string in the order of appearance.
+4. If the number of unique strings is less than k, return an empty string.
+
+Sample Test Case 0:
+Input:
+3
+dac
+ba
+a
+1
+
+Output:
+dac
+
+Explanation:
+All the strings in the input are unique: "dac", "ba", "a".
+As k = 1, the first unique string is "dac".
+
+Sample Test Case 1:
+Input:
+6
+d
+b
+c
+b
+c
+a
+2
+
+Output:
+a
+
+Explanation:
+The distinct strings are "d" and "a" in order of appearance.
+The second unique string is "a", which is returned since k = 2.
+*/
+
+
+
 using System;
 using System.Collections.Generic;
 using System.IO;
